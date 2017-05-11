663c88bc30fa7dc61cb45cbf4d089512d7f762c1
dkaluder <dub.kaluder@gmail.com>
2017-05-11 22:25:44 +0200

    izmjena panela

---
+++ paneli.js
@@ -0,0 +1,17 @@
+<script> function evenpanels() {
+			var heights = [];                           // make an array
+			$(".panel-body").each(function(){           // copy the height of each
+				heights.push($(this).height());         //   element to the array
+			});
+			heights.sort(function(a, b){return b - a}); // sort the array high to low
+			var minh = heights[0];                      // take the highest number    
+			$(".panel-body").height(minh);              // and apply that to each element
+		}
+		
+		$(window).resize(function () { 
+        $(".panel-body").each(function(){
+            $(this).css('height',"");           // clear height values
+        });                                     
+        evenpanels();
+});
+</script>
