fee6058c2166a7bd192ed4bd56cf8ecc07027196
dkaluder <dub.kaluder@gmail.com>
2017-05-13 20:18:24 +0200

    delete dbcom

--- dbconfig.php
+++ dbconfig.php
@@ -27,13 +27,3 @@ class Database
     }
 }
 ?>
-
-// PHP Data Objects(PDO) Sample Code:
-		try {
-			$dbc = new PDO("sqlsrv:server = tcp:pwa.database.windows.net,1433; Database = Autotrader", "dkaluder", "Cbb446bb");
-			$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		}
-		catch (PDOException $e) {
-			print("Error connecting to SQL Server.");
-			die(print_r($e));
-		}
