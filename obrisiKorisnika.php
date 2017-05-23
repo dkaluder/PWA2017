763c6e33ab8b724ce38c32fed2ed0f9ce5916808
dkaluder <dub.kaluder@gmail.com>
2017-05-24 00:25:27 +0200

    limit

--- obrisiKorisnika.php
+++ obrisiKorisnika.php
@@ -2,7 +2,7 @@
 	require_once 'class.user.php';
 	$user = new USER();
 	$id = $_POST['ID_delete'];	
-	$stmt = $user->runQuery("DELETE FROM tbl_users WHERE userID = " . $id . " LIMIT 1;");
+	$stmt = $user->runQuery("DELETE FROM tbl_users WHERE userID = " . $id . " ;");
 	$stmt->execute();
 	header("Location: korisnici.php");
 ?>
