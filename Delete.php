17a16ab3671e9717456c3ba809384f414c3983d4
dkaluder <dub.kaluder@gmail.com>
2017-05-23 23:04:54 +0200

    del

--- Delete.php
+++ Delete.php
@@ -1,10 +1,10 @@
 <?php 
 	include "database.php";	
 	$id = $_POST['ID_delete'];
-	$queryGetImage="SELECT oglas.URLSlike FROM oglas where oglas.id = ".$id." LIMIT 1;";
+	/*$queryGetImage="SELECT oglas.URLSlike FROM oglas where oglas.id = ".$id." LIMIT 1;";
 	$red = $dbc->query($queryGetImage);
 	$column = $red->fetchColumn();
-	unlink('Img/'.$column);
+	unlink('Img/'.$column);*/
 	$query = "DELETE FROM oglas WHERE ID = " . $id . " LIMIT 1;";
 	$result = $dbc->query($query);		
 	$result = null;
